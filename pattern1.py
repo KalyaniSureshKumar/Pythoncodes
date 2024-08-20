57e78a0332ecaa92d9b1ce4afb970957c00d25dc
KalyaniSureshKumar <144218661+KalyaniSureshKumar@users.noreply.github.com>
2024-08-20 21:09:47 +0530

    Add files via upload

---
+++ pattern1.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug 20 21:08:30 2024
+
+@author: JAYAPRAKASH L V
+"""
+
+n = int(input("Enter the number of rows: "))  
+
+ 
+for num in range(n+1):  
+    
+        for i in range(num):  
+            print(num, end=" ")       
+  
+        
+        print()
